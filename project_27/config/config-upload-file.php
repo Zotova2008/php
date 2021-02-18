b7393dc89441494d40498250eb7d8768b06020b5
Natali <n.z.work@yandex.ru>
2021-02-19 00:05:59 +0300

    Модуль 27

--- project_27/config/config-upload-file.php
+++ project_27/config/config-upload-file.php
@@ -1,4 +1,8 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+
+
 // Массив ошибок
 $errors = [];
 // Массив сообщений
@@ -6,11 +10,13 @@ $messages = [];
 
 // Если файл был отправлен
 if (!empty($_FILES)) {
-
+  print_r($_FILES['files']);
   // Проходим в цикле по файлам
   for ($i = 0; $i < count($_FILES['files']['name']); $i++) {
 
     $fileName = $_FILES['files']['name'][$i];
+    $fileNameCmps = explode(".", $fileName);
+    $fileExtension = strtolower(end($fileNameCmps));
 
     // Проверяем размер
     if ($_FILES['files']['size'][$i] > UPLOAD_MAX_SIZE) {
@@ -24,17 +30,24 @@ if (!empty($_FILES)) {
       continue;
     }
 
-    $filePath = UPLOAD_DIR . '/' . basename($fileName);
+    $filePath = UPLOAD_DIR . '/' . time() . '.' . $fileExtension;
 
     // Пытаемся загрузить файл
     if (!move_uploaded_file($_FILES['files']['tmp_name'][$i], $filePath)) {
       $errors[] = 'Ошибка загрузки файла ' . $fileName;
       continue;
+    } else {
+      require 'connect.php';
+      $login_user = $_SESSION['user']['login'];
+      $img_name = $_FILES['files']['name'][$i];
+      mysqli_query($connect, "INSERT INTO images (login, name, path) VALUES ('$login_user', '$img_name', '$filePath')");
     }
   }
 
   if (empty($errors)) {
     $messages[] = 'Файлы были загружены';
+    header('Location: ../index.php');
+    exit();
   }
 }
 
